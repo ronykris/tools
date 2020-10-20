5ea7f150e97af5a264684fed23bc9da84df00424
ronykris <rony.kris@gmail.com>
2020-10-20 19:03:34 +0000

    initial update

---
+++ fuzzer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TOOLS=$(pwd)
+inputfile = $1
+while IFS= read -r line
+do
+	#echo $line | tee -a $1.ffuf	
+	ffuf -u "$line/FUZZ" -w $TOOLS/SecLists/Discovery/Web-Content/common.txt | tee -a $1.ffuf
+done < "$1"
